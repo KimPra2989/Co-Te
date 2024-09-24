d3ead2d79b6ea57f0b5120545e26397996968398
kimpra <dustmqaksdltkfrlfdldi@gmail.com>
2024-09-24 19:45:52 +0900

    js 솔루션 파일 랜더링 확인

--- app/fns/getMD.ts
+++ app/fns/getMD.ts
@@ -1,11 +1,8 @@
 import fs from 'fs'
 import path from 'path'
 
-const getMD = () => {
-  const filePath = path.join(
-    process.cwd(),
-    '프로그래머스/3/118668. 코딩 테스트 공부/README.md'
-  )
+const getMD = (pathname: string) => {
+  const filePath = path.join(process.cwd(), pathname)
   const fileContents = fs.readFileSync(filePath, 'utf8')
   return fileContents
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,37 @@ import getMD from './fns/getMD'
 import rehypeRaw from 'rehype-raw'
 
 export default function HomePage() {
-  const data = getMD()
+  const data = getMD('프로그래머스/3/118668. 코딩 테스트 공부/README.md')
+  const solution_code = getMD(
+    '프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.js'
+  )
   // console.log(data)
+  const flexStyles = {
+    display: 'flex',
+    height: '100vh',
+  }
+  const rightStyles = {
+    flex: 1,
+    // backgroundColor: 'pink',
+    color: 'white',
+    whiteSpace: 'pre-wrap',
+    overflowY: 'auto',
+
+  }
+  const leftStyles = {
+    width: '40%',
+    overflowY: 'auto',
+    padding: '20px',
+  }
 
   return (
-    <div>
-      <Markdown rehypePlugins={[rehypeRaw]}>{data}</Markdown>
-      {/* <Markdown rehypePlugins={[rehypeRaw]}>{markdownText}</Markdown> */}
+    <div style={flexStyles}>
+      <div style={leftStyles}>
+        <Markdown rehypePlugins={[rehypeRaw]}>{data}</Markdown>
+      </div>
+      <div style={rightStyles}>
+        <code>{solution_code}</code>
+      </div>
     </div>
   )
 }
